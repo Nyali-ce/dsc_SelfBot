39fbe8d01201f498368130e99c9ed8908c290bea
Alice Beaumier <nathan@alconat.ca>
2023-01-30 14:20:57 -0500

    Updated | ping commands actually shows ping now :)

--- src/commands/info/ping.ts
+++ src/commands/info/ping.ts
@@ -7,12 +7,10 @@ export default {
     run: async (client: any, message: any, args: any) => {
         const startTime = Date.now()
 
-        await sendMessage(message.channel_id, 'Pong!');
+        const res = await sendMessage(message.channel_id, 'Pinging...')
 
         const endTime = Date.now()
 
-        const timeDifference = endTime - startTime;
-
-        editMessage(message.channel_id, message.id, `Pong! Took ${timeDifference}ms to respond.`)
+        await editMessage(message.channel_id, res.id, `Pong! \`${endTime - startTime}ms\``)
     },
 }
