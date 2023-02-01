afe81c5985440692f6843621d8fba0e4c802b792
Nyali.ce <nathan@alconat.ca>
2023-02-01 11:14:05 -0500

    updated | stop command

--- src/commands/✨test/stop.ts
+++ src/commands/✨test/stop.ts
@@ -1,8 +1,12 @@
+import sendMessage from "../../functions/api/sendMessage.js";
+
 export default {
     name: 'stop',
     description: 'Stops the bot',
     permissions: ['OWNER'],
     run: async (client: any, message: any, args: any) => {
+        await sendMessage(message.channel_id, `Stopping \`pid${process.pid}\` on \`${process.env.OS}\``);
+
         client.ws.close();
         process.exit();
     }
