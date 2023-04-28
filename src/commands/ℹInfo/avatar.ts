8b9a29a05ae212af25a7217cf6e9be62acb64f00
Alice Beaumier <nathan@alconat.ca>
2023-04-28 14:44:40 -0400

    removed useless data

--- src/commands/ℹInfo/avatar.ts
+++ src/commands/ℹInfo/avatar.ts
@@ -5,11 +5,11 @@ export default {
     description: 'Get a user\'s avatar.',
     permission: 'ADMINISTRATOR',
     run: async (client: any, message: any, args: any) => {
-        const userData = message.mentions[0] || message.author
+        const userData: any = message.mentions[0] || message.author
 
-        const user = new User({ id: userData.id, avatar: userData.avatar });
+        const user: User = new User({ id: userData.id, avatar: userData.avatar });
 
-        const avatar = user.avatar
+        const avatar: string | undefined = user.avatar
             ? user.displayAvatarURL(1024)
             : undefined;
 
